74cb3ce1157c3d77c4841fa6fef020cfdbc55d0d
Yeeon Ryu <fbdpdjs0720@gmail.com>
2025-10-30 13:55:56 -0500

    added course-info page

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import {LoginPage} from './login-page/login-page';
 import {RegisterPage} from './register-page/register-page';
 import {QuestionnairePage} from './questionnaire/questionnaire';
 import {ProfilePage} from './profile/profile';
+import {CourseInfoPage} from './course-info/course-info';
 
 export const routes: Routes = [
   {
@@ -30,5 +31,11 @@ export const routes: Routes = [
   {
     path: 'profile',
     component: ProfilePage
+  },
+  {
+    path: 'course',
+    component: CourseInfoPage
   }
+
+
 ];
