9e99f9cf60d635951b0a8cbee41b208022b73cf6
lockermanwxlf <lockermanwxlf@gmail.com>
2025-10-30 11:43:48 -0500

    Add migrations for database

---
+++ courses-scraping/courses_scraping/persistence/__init__.py
@@ -0,0 +1,6 @@
+from courses_scraping.persistence.base import Base
+from courses_scraping.persistence.course import Course
+from courses_scraping.persistence.courseoffering import CourseOffering
+from courses_scraping.persistence.instructor import Instructor
+
+__all__ = ['Base', 'Course', 'CourseOffering', 'Instructor']
