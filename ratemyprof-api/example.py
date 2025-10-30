bfe972fe3e05b20b7c8fb0a5a7a65d8ff72a0084
Yeeon Ryu <fbdpdjs0720@gmail.com>
2025-10-30 09:18:49 -0500

    removed old ratemyprof api and added the updated version

--- ratemyprof-api/example.py
+++
@@ -1,8 +0,0 @@
-from ratemyprof_api.ratemyprof_api import RateMyProfApi  # or whatever class/function is defined
-
-def main():
-    api = RateMyProfApi()
-    print(api.RateMyProfScraper(850))  # example usage
-
-if __name__ == "__main__":
-    main()
