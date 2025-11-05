469778a83b7452ff164149c7b5ab5faeb18b9d20
Jenni Oishee <jennioishee@Jennis-MacBook-Pro.local>
2025-11-04 19:38:15 -0600

    Add courses route to fix Start Planning button navigation

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LandingPage } from './landing-page/landing-page';
 import { LoginPage } from './login-page/login-page';
 import { RegisterPage } from './register-page/register-page';
 import { QuestionnairePage } from './questionnaire/questionnaire';
+import { CoursesPage } from './courses-page/courses-page';
 
 export const routes: Routes = [
   {
@@ -25,5 +26,9 @@ export const routes: Routes = [
   {
     path: 'questionnaire',
     component: QuestionnairePage,
+  },
+  {
+    path: 'courses',
+    component: CoursesPage,
   }
 ];
