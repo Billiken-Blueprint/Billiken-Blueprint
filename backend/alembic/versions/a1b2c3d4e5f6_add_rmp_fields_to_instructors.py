dd3a09be8ac79a05e53fc485eea882c2f064a962
Yeeon Ryu <fbdpdjs0720@gmail.com>
2025-11-22 14:59:17 -0600

    Fix migration chain: RMP fields migration should come after saved_course_codes

--- backend/alembic/versions/a1b2c3d4e5f6_add_rmp_fields_to_instructors.py
+++ backend/alembic/versions/a1b2c3d4e5f6_add_rmp_fields_to_instructors.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1b2c3d4e5f6'
-down_revision: Union[str, Sequence[str], None] = '6f6c50928852'
+down_revision: Union[str, Sequence[str], None] = '8407d5169b83'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
